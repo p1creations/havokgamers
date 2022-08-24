87b8c254023a53c2f8721d57a63638259b4ea3ba
p1creations <47378008+p1creations@users.noreply.github.com>
2022-08-24 12:22:02 -0500

    Update rand.js
    need to finish sitlo's

--- assets/rand.js
+++ assets/rand.js
@@ -1,12 +1,12 @@
-var casFeat = '<iframe src="https://player.twitch.tv/?video=v913456125&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var daddyFeat = '<iframe src="https://player.twitch.tv/?video=v1108705814&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var eirFeat = '<iframe src="https://clips.twitch.tv/embed?clip=EagerAssiduousSushiBuddhaBar-gRimpZBSFn_UHgsG&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var kralkFeat = '<iframe src="https://clips.twitch.tv/embed?clip=FlirtyInterestingArmadilloFeelsBadMan-0pA31RU8DfzC8y-T&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var p1Feat = '<iframe src="https://player.twitch.tv/?video=v1218012018&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var shelbyFeat = '<iframe src="https://clips.twitch.tv/embed?clip=HungrySpookyCougarDeIlluminati-JG-vgKszrWAvjLqi&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var shipFeat = '<iframe src="https://clips.twitch.tv/embed?clip=CulturedCulturedHyenaDancingBanana-Exd5W0ju5qrYW8R_&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var skyFeat = '<iframe src="https://player.twitch.tv/?video=v1057407054&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
-var sugarFeat = '<iframe src="https://clips.twitch.tv/embed?clip=ResourcefulEnthusiasticFriseeHoneyBadger-uslnIn1EjpCwqO5H&parent=www.skateorsplit.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var casFeat = '<iframe src="https://player.twitch.tv/?video=v913456125&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var daddyFeat = '<iframe src="https://player.twitch.tv/?video=v1108705814&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var eirFeat = '<iframe src="https://clips.twitch.tv/embed?clip=EagerAssiduousSushiBuddhaBar-gRimpZBSFn_UHgsG&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var kralkFeat = '<iframe src="https://clips.twitch.tv/embed?clip=FlirtyInterestingArmadilloFeelsBadMan-0pA31RU8DfzC8y-T&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var p1Feat = '<iframe src="https://player.twitch.tv/?video=v1218012018&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var shelbyFeat = '<iframe src="https://clips.twitch.tv/embed?clip=HungrySpookyCougarDeIlluminati-JG-vgKszrWAvjLqi&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var shipFeat = '<iframe src="https://clips.twitch.tv/embed?clip=CulturedCulturedHyenaDancingBanana-Exd5W0ju5qrYW8R_&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var skyFeat = '<iframe src="https://player.twitch.tv/?video=v1057407054&parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
+var sitloFeat = '<iframe src="https://clips.twitch.tv/embed?clip=parent=www.havokgamers.com&autoplay=true" height="500" width="100%" allowfullscreen="true"></iframe>';
 
 function myFunction() {
   var rand = Math.floor((Math.random() * 9) + 1);
@@ -18,7 +18,7 @@ function myFunction() {
   if(rand === 6) { rand = shelbyFeat;}
   if(rand === 7) { rand = shipFeat;}
   if(rand === 8) { rand = skyFeat;}
-  if(rand === 9) { rand = sugarFeat;}
+  if(rand === 9) { rand = sitloFeat;}
 
 
   document.getElementById("featuredContent").innerHTML = rand;
